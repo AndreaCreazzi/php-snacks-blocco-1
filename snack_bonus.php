eb539d5cb1b6966b24e0d91154ceecbf63f3cd01
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-17 15:31:32 +0200

    fix snack_bonus

--- snack_bonus.php
+++ snack_bonus.php
@@ -1,6 +1,6 @@
 <?php
 
-$paragrafo_lungo = "Questo è un paragrafo abbastanza lungo. Esso contiene diverse frasi. Prendiamo il paragrafo e suddividiamolo in tanti paragrafi <p>. Ogni punto segna l'inizio di un nuovo paragrafo. Questa è un'altra frase che fa parte del paragrafo.";
+$paragrafo_lungo = "Questo è un paragrafo abbastanza lungo. Esso contiene diverse frasi. Prendiamo il paragrafo e suddividiamolo in tanti paragrafi. Ogni punto segna l'inizio di un nuovo paragrafo. Questa è un'altra frase che fa parte del paragrafo.";
 
 $frasi = explode('.', $paragrafo_lungo);
 
